137ef54fc0846a75911257666e35b2d652e54a4a
Francois Roland <froland@froland.be>
2023-05-24 14:45:08 +0200

    Generate project with spring initializr.

---
+++ src/main/java/be/eafcuccle/projint/debugger2/ProjintDebugger2Application.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.debugger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjintDebugger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjintDebugger2Application.class, args);
+	}
+
+}
